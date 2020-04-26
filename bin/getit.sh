674b4701e281349e5211a4cee2ddcc9a68b4ba87
Fabrice Niessen <fniessen@pirilampo.org>
2020-04-26 21:31:58 +0200

    Remove useless files (moved to another repository)

--- bin/getit.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-# mike's getit.sh script
-if [ -z $1 ]; then
-    echo -e "\n\tUsage: $0 <host> <URL>"
-    exit
-fi
-echo -e "GET $2 HTTP/1.0\n\n" | \
-nc -vv $1 80
--- bin/sgetit.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-# mike's sgetit.sh script
-if [ -z $1 ]; then
-    echo -e "\n\tUsage: $0 <SSL host> <URL>"
-    exit
-fi
-echo -e "GET $2 HTTP/1.0\n\n" | \
-openssl s_client -quiet -connect $1:443 2>/dev/null
